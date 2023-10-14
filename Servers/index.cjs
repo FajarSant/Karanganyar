9152b5800ab3a21e43c04c3a35604a76a6d44470
FajarSant <4jar100so@gmail.com>
2023-10-14 12:19:49 +0700

    Perbaikan page

--- Servers/index.cjs
+++ Servers/index.cjs
@@ -4,9 +4,15 @@ const cheerio = require('cheerio');
 const Sentiment = require('sentiment');
 
 
-
 const app = express();
 const port = 4001;
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 // Rute untuk URL http://localhost:4001/list
 app.get("/list", async (req, res) => {
