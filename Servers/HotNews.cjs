5ac5a04f0b90ca8de65514119dce965eeec239bc
FajarSant <4jar100so@gmail.com>
2023-10-14 12:41:48 +0700

    Perbaikan page

--- Servers/HotNews.cjs
+++ Servers/HotNews.cjs
@@ -1,12 +1,12 @@
-const express = require('express');
-const axios = require('axios');
-const cheerio = require('cheerio');
-const Sentiment = require('sentiment'); // Import pustaka analisis sentimen
+import express from 'express';
+import axios from 'axios';
+import cheerio from 'cheerio';
+import Sentiment from 'sentiment';
+import { send } from '@vercel/node'; // Import modul send dari Vercel
 
 const app = express();
-const port = 4003; // Ganti dengan port yang Anda inginkan
+const port = 4003;
 
-// Middleware untuk mengizinkan permintaan lintas domain (CORS)
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
@@ -14,21 +14,18 @@ app.use((req, res, next) => {
   next();
 });
 
-// Endpoint untuk melakukan scraping dan mengambil data berita
 app.get('/HotNews', (req, res) => {
   const url = 'https://www.detik.com/tag/bupati-karanganyar?tag_from=karanganyar';
 
   axios.get(url)
-    .then((response) => {
+    .then(async (response) => {
       const html = response.data;
       const $ = cheerio.load(html);
       
-      // Ubah selektor untuk mencari elemen-elemen berita
       const beritaElements = $('.l_content article');
 
       const beritaData = [];
 
-      // Inisialisasi pustaka analisis sentimen
       const sentiment = new Sentiment();
 
       beritaElements.each((index, element) => {
@@ -37,10 +34,8 @@ app.get('/HotNews', (req, res) => {
         const tanggal = $(element).find('.date').text();
         const isi = $(element).find('p').text();
 
-        // Menambahkan kode HTML untuk elemen gambar di sini
         const gambarURL = $(element).find('span.ratiobox_content img').attr('src');
 
-        // Analisis sentimen pada deskripsi berita
         const analisisDeskripsi = sentiment.analyze(isi);
 
         beritaData.push({
@@ -49,20 +44,20 @@ app.get('/HotNews', (req, res) => {
           isi,
           beritaLink,
           gambarURL,
-          sentimen: analisisDeskripsi, // Menambahkan hasil analisis sentimen ke data berita
+          sentimen: analisisDeskripsi,
         });
       });
 
-      res.json(beritaData);
+      // Menggunakan modul send untuk mengirim respons JSON
+      send(res, 200, beritaData);
     })
     .catch((error) => {
       console.error('Gagal mengambil halaman web:', error);
-      res.status(500).json({ error: 'Terjadi kesalahan' });
+      // Menggunakan modul send untuk mengirim respons kesalahan
+      send(res, 500, { error: 'Terjadi kesalahan' });
     });
 });
 
-// Mulai server backend
 app.listen(port, () => {
   console.log(`Server backend is running on port ${port}`);
 });
-  
